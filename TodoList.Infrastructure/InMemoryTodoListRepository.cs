1941020768e336470a379c0637d2fef57af772b0
Wells <ll0918900@gmail.com>
2024-01-24 08:00:03 +0800

    test: 將整合測試的 Repository 抽換成 InMemory

---
+++ TodoList.Infrastructure/InMemoryTodoListRepository.cs
@@ -0,0 +1,36 @@
+﻿using DM = TodoList.Domain;
+
+namespace TodoList.Infrastructure;
+
+public class InMemoryRepository : DM.ITodoListRepository
+{
+    private static readonly Dictionary<Guid, Domain.TodoList> TodoListMap = new();
+
+    public DM.TodoList? Get(Guid listId)
+    {
+        if (TodoListMap.TryGetValue(listId, out var todoList))
+        {
+            return todoList;
+        }
+
+        return null;
+    }
+
+    public Task Create(DM.TodoList todoList)
+    {
+        TodoListMap.Add(todoList.Id, todoList);
+        return Task.CompletedTask;
+    }
+
+    public Task Update(DM.TodoList todoList)
+    {
+        TodoListMap[todoList.Id] = todoList;
+        return Task.CompletedTask;
+    }
+
+    public Task Delete(DM.TodoList todoList)
+    {
+        TodoListMap.Remove(todoList.Id);
+        return Task.CompletedTask;
+    }
+}
--- TodoList.IntegrationTests/IntegrationTestServer.cs
+++ TodoList.IntegrationTests/IntegrationTestServer.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using TodoList.Domain;
+using TodoList.Infrastructure;
 
 namespace TodoList.IntegrationTests;
 
 public class IntegrationTestServer : WebApplicationFactory<Program>
 {
     public HttpClient Client { get; }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddScoped<ITodoListRepository, InMemoryRepository>();
+        });
+    }
 
     public IntegrationTestServer()
     {
